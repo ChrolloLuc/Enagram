6d0b34cb321a9fcc34e3abf13f58b7732d080ddd
ChrolloLuc <154702336+ChrolloLuc@users.noreply.github.com>
2025-09-07 18:15:06 +0400

    davamate martivi shedarebis logika

--- src/Compare/Compare.jsx
+++ src/Compare/Compare.jsx
@@ -1,6 +1,44 @@
+import { useState } from "react"
 import styles from "./CompareStyles.module.css"
 
+
+
+
+
+
 function Compare() {
+
+  const [oldText, setOldText] = useState("")
+  const [newText, setNewText] = useState("")
+  const [diff, setDiff] = useState(null)
+  const result = []
+
+
+  const handleButtonClick = () =>{
+    const oldTextComapre = oldText.split(" ")
+    const newTextComapre = newText.split(" ")
+    // console.log(oldTextComapre)
+    // console.log(newTextComapre)
+
+    for (let i = 0; i<Math.max(oldTextComapre.length, newTextComapre.length); i++){
+      if(oldTextComapre[i]===newTextComapre[i]){
+        result.push(<span key={i}>{oldTextComapre[i]}</span>)
+        console.log("everything is fine")
+      } else if(oldTextComapre.length>newTextComapre.length){
+        result.push(<span key={`old-${i}`} style={{color: "red"}}>{oldTextComapre[i]} </span>)
+        console.log("it should be red")
+      } else {
+        result.push(<span key={`new-${i}`} style={{color: "green"}}>{newTextComapre[i]} </span>)
+        console.log("it should be green")
+      }
+    }
+    setDiff(result)
+  }
+
+  
+
+
+
   return (
     <>
         <section className={styles.container}>
@@ -18,10 +56,25 @@ function Compare() {
             </div>
 
             <div className={styles.textContainer}>
-              <textarea placeholder="დაიწყე წერა..." className={styles.textInput}></textarea>
+              <textarea 
+              placeholder="დაიწყე წერა..." 
+              className={styles.textInput} 
+              value={oldText} 
+              onChange={(e)=>setOldText(e.target.value)}
+              ></textarea>
                 <p>&hArr;</p>
-              <textarea placeholder="დაიწყე წერა..." className={styles.textInput}></textarea>
+              <textarea 
+              placeholder="დაიწყე წერა..." 
+              className={styles.textInput} 
+              value={newText}
+              onChange={(e)=>setNewText(e.target.value)}
+              ></textarea>
             </div>
+            <div className={styles.compareButton}>
+              <button onClick={handleButtonClick}>შედარება</button>
+              {diff}
+            </div>
+              
         </section>
     </>
   )
